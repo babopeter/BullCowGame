94860e236f4197f746bc1c9729b741d6ae45857e
Peter Babo <babopeter1@gmail.com>
2020-06-24 16:00:45 +0200

    Added basic game loop
    Basice game loop done

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,10 +3,30 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    // START GAME
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull Cows Game!"));
+    PrintLine(TEXT("Please guess the four letter word.")); // 4 letter word hardcoded
+    PrintLine(TEXT("Input something and press Enter..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    // SET HIDDEN WORD
+    FString HiddenWord = TEXT("cake"); // Move outside this function
+    
+    //PROMPT FOR GUESS
+    PrintLine(Input);
+    
+    //IS GUESS CORRECT
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have lost!"));
+    }
+    //WIN/LOSE
 }
